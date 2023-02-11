9cc45d28d9b1868dab3b3698c631fe143ae31a36
Daniel Vilas <danielvilasgonzalez@gmail.com>
2023-02-10 18:08:22 -0800

    loop over sp

--- Scripts/04_Bering10K_data.R
+++ Scripts/04_Bering10K_data.R
@@ -175,10 +175,14 @@ for (y in sta_y:end_y) {
     f<-files.for[1]
     file.id<-nc_forfiles[which(nc_forfiles$name==f),'id']} #for year>2020 have to select projection
   
-  #download file into temp folder
-  googledrive::drive_download(file=file.id$id,
-                              path = paste0('./Data/Bering 10K ROMS/',f),
-                              overwrite = TRUE)
+  #if not file, download
+  if (!(f %in% list.files('./Data/Bering 10K ROMS/'))) {
+    
+    #download file into temp folder
+    googledrive::drive_download(file=file.id$id,
+                                path = paste0('./Data/Bering 10K ROMS/',f),
+                                overwrite = TRUE)
+  }
   
   #open netcdf
   nc<-nc_open(paste0('./Data/Bering 10K ROMS/',f))
@@ -256,3 +260,165 @@ for (y in sta_y:end_y) {
 
 #save grid Bering Sea with SBT and depth as dataframe
 saveRDS(grid.ebs_year,'./slope shelf EBS NBS VAST/grid_ebs_covariate_data.rds')
+
+#####################################
+# LOOP OVER SPP
+#####################################
+
+#get species name
+splist<-list.dirs('./slope shelf EBS NBS VAST',full.names = FALSE)
+splist<-splist[-1]
+
+#loop over species to add SBT to data_geostat
+for (sp in splist) {
+  
+  #sp<-splist[3]
+  
+  #print species to check progress
+  cat(paste(" ############# ", sp, " #############\n"))
+  
+  #open data_geostat
+  df1<-readRDS(paste0('./slope shelf EBS NBS VAST/',sp,'/data_geostat.rds'))
+  
+  #create df to store results
+  df1_temp<-data.frame(matrix(nrow=0,
+                              ncol=ncol(df1)+1))
+  colnames(df1_temp)<-c("Species","Year","Lat","Lon","CPUE_kg","Survey","Depth","BottomTempROMS")
+  
+  #sort years
+  years<-sort(unique(df1$Year))
+  
+  for (y in min(years):max(years)) {
+    
+    #y<-2020
+    
+    #print year to check progress
+    cat(paste("    ---- year", y, "----\n"))
+    
+    #subset df by year
+    df2<-subset(df1,Year==y)
+    
+    #if no data for that year, add NA in biomass estimates because covariate data needs SBT for all years
+    if (nrow(df2)==0) {
+      df2<-subset(df1,Year==y-1)
+      df2$Year<-y
+      df2$CPUE_kg<-NA
+    }
+    
+    #open netcdf file for each year
+    if (y %in% c(1980:1984)) {
+      f<-files.hist[1]
+      file.id<-nc_histfiles[which(nc_histfiles$name==f),'id']
+    } else  if (y %in% c(1985:1989)) {
+      f<-files.hist[2]
+      file.id<-nc_histfiles[which(nc_histfiles$name==f),'id']
+    } else  if (y %in% c(1990:1994)) {
+      f<-files.hist[3]
+      file.id<-nc_histfiles[which(nc_histfiles$name==f),'id']
+    } else  if (y %in% c(1995:1999)) {
+      f<-files.hist[4]
+      file.id<-nc_histfiles[which(nc_histfiles$name==f),'id']
+    } else if (y %in% c(2000:2004)) {
+      f<-files.hist[5]
+      file.id<-nc_histfiles[which(nc_histfiles$name==f),'id']
+    } else if (y %in% c(2005:2009)) {
+      f<-files.hist[6]
+      file.id<-nc_histfiles[which(nc_histfiles$name==f),'id']
+    } else if (y %in% c(2010:2014)) {
+      f<-files.hist[7]
+      file.id<-nc_histfiles[which(nc_histfiles$name==f),'id']
+    } else if (y %in% c(2015:2019)) {
+      f<-files.hist[8]
+      file.id<-nc_histfiles[which(nc_histfiles$name==f),'id']
+    } else if (y %in% c(2020)) {
+      f<-files.hist[9]
+      file.id<-nc_histfiles[which(nc_histfiles$name==f),'id']
+    } else if (y %in% c(2021:2022)) {
+      f<-files.for[1]
+      file.id<-nc_forfiles[which(nc_forfiles$name==f),'id']} #for year>2020 have to select projection
+    
+    #if not file, download
+    # if (!(f %in% list.files('./Data/Bering 10K ROMS/'))) {
+    #   
+    #   #download file into temp folder
+    #   googledrive::drive_download(file=file.id$id,
+    #                               path = paste0('./Data/Bering 10K ROMS/',f),
+    #                               overwrite = TRUE)
+    # }
+    
+    #open netcdf
+    nc<-nc_open(paste0('./Data/Bering 10K ROMS/',f))
+    
+    #dimensions netcdf file
+    #258 rows
+    #182 cols
+    #46956 cells
+    #259 time steps
+    
+    #get variables
+    #names(nc$var)
+    
+    #get latitude
+    lats <- ncvar_get(nc,"lat_rho")
+    
+    #get longitude
+    lons <- ncvar_get(nc,"lon_rho")
+    
+    #get SBT
+    temp<-ncvar_get(nc,'temp')
+    
+    #get time
+    t_axis<-ncvar_get(nc,"ocean_time")
+    
+    #convert time
+    time_axis <- as.POSIXct(t_axis, origin = "1900-01-01", tz = "GMT") 
+    
+    #get weekly temp slices from specific year y
+    nc_y<-ncvar_get(nc, "temp")[,,which(grepl(paste0(y,'-'),time_axis))]
+    
+    #get mean matrix for this year
+    mean_nc<-apply(nc_y,c(1,2),mean,na.rm=TRUE)
+    
+    #create dataframe with lats, lons and mean year SBT
+    df_nc<-data.frame(Lat=as.vector(lats),
+                      Lon=as.vector(lons),
+                      temp=as.vector(mean_nc))
+    
+    #longitude are in eastern. get SBT for the western hemisphere (Bering Sea). longitude greater than 180 degrees
+    df_nc1<-df_nc[which(df_nc$Lon>=180),]
+    
+    #convert eastern longitude to western values (higher). longitude should be negative
+    df_nc1$Lon<-df_nc1$Lon-180-180
+    
+    #filter values from the grid 
+    df_nc2<-subset(df_nc1,Lat >= min(df1$Lat) & Lat <= max(df1$Lat) & Lon >= min(df1$Lon) & Lon <= max(df1$Lon))
+    
+    #remove NA rows
+    df_nc3<-df_nc2[complete.cases(df_nc2),]
+    
+    #create a spatial object
+    coordinates(df_nc3) <- ~ Lon + Lat
+    #plot(df_nc3)
+    #as.data.frame(df_nc)$temp[nc_index]
+    spg <- df2
+    coordinates(spg) <- ~ Lon + Lat
+    
+    #get the nearests points from one df to other df
+    nn<-get.knnx(coordinates(df_nc3),coordinates(spg),1)
+    nc_index<-nn$nn.index[,1]
+    
+    #get SBT
+    temps<-as.data.frame(df_nc3)$temp[nc_index]
+    df2$BottomTempROMS<-temps
+    
+    #add results
+    df1_temp<-rbind(df1_temp,df2)
+    
+  }
+  
+  #save data_geostat with SBT
+  saveRDS(df1_temp,
+          paste0('./slope shelf EBS NBS VAST/',sp,'/data_geostat_temp.rds'))
+  
+}
+
