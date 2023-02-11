b4c21fb47a2de6bf7890c76e0a452c07dda74073
Daniel Vilas <danielvilasgonzalez@gmail.com>
2023-02-10 17:57:38 -0800

    grid loop ROMS

--- Scripts/04_Bering10K_data.R
+++ Scripts/04_Bering10K_data.R
@@ -85,3 +85,174 @@ id.data<-files.1[which(files.1$name %in% grfiles),]
  
 #join grids
 grid.ebs<-rbind(EBSshelf_gr,EBSslope_gr,NBS_gr)
+
+#####################################
+# ADD DEPTH
+#####################################
+
+#create directory
+dir.create('./Data/Bathymetry/',showWarnings = FALSE)
+
+#get raster depth from gebco (https://download.gebco.net/)
+#get id shared folder from google drive
+id.bering.folder<-files[which(files$name=='Bathymetry'),'id']
+
+#list of files and folder
+files.1<-googledrive::drive_ls(id.bering.folder$id)
+id.data<-files.1[which(files.1$name=='gebco_2022_n70.0_s50.0_w-180.0_e-155.0.asc'),]
+
+googledrive::drive_download(file=id.data$id,
+                            path = paste0('./Data/Bathymetry/',id.data$name),
+                            overwrite = TRUE)
+
+#read raster
+r<-raster('./Data/Bathymetry/gebco_2022_n70.0_s50.0_w-180.0_e-155.0.asc')
+
+#extract depth values for each station
+rr<-extract(r, SpatialPoints(cbind(grid.ebs$Lon,grid.ebs$Lat)))
+grid.ebs$DepthGEBCO<-rr
+
+#####################################
+# LOOP OVER YEARS
+#####################################
+
+#get id shared folder from google drive
+id.roms.folder<-files[which(files$name=='Bering 10K ROMS'),'id']
+
+#list of files and folder of netcd files
+files.1<-googledrive::drive_ls(id.roms.folder$id)
+id.data<-files.1[which(files.1$name=='netcdf_historical'),'id']
+nc_histfiles<-googledrive::drive_ls(id.data$id)
+id.data<-files.1[which(files.1$name=='netcdf_forecast'),'id']
+nc_forfiles<-googledrive::drive_ls(id.data$id)
+files.hist<-sort(nc_histfiles$name)
+files.for<-sort(nc_forfiles$name)
+
+#create df to store results
+grid.ebs_year<-data.frame(matrix(nrow = 0,
+                                 ncol = ncol(grid.ebs)+2))
+colnames(grid.ebs_year)<-c(colnames(grid.ebs),
+                           'BottomTempROMS',
+                           'Year')
+
+#loop over years to incorporate values into the Bering Sea grid
+for (y in sta_y:end_y) {
+  
+  #y<-2020
+  
+  #print year to check progress
+  cat(paste("    ----- year", y, "-----\n"))  
+  
+  #open netcdf file for each year
+  if (y %in% c(1980:1984)) {
+    f<-files.hist[1]
+    file.id<-nc_histfiles[which(nc_histfiles$name==f),'id']
+  } else  if (y %in% c(1985:1989)) {
+    f<-files.hist[2]
+    file.id<-nc_histfiles[which(nc_histfiles$name==f),'id']
+  } else  if (y %in% c(1990:1994)) {
+    f<-files.hist[3]
+    file.id<-nc_histfiles[which(nc_histfiles$name==f),'id']
+  } else  if (y %in% c(1995:1999)) {
+    f<-files.hist[4]
+    file.id<-nc_histfiles[which(nc_histfiles$name==f),'id']
+  } else if (y %in% c(2000:2004)) {
+    f<-files.hist[5]
+    file.id<-nc_histfiles[which(nc_histfiles$name==f),'id']
+  } else if (y %in% c(2005:2009)) {
+    f<-files.hist[6]
+    file.id<-nc_histfiles[which(nc_histfiles$name==f),'id']
+  } else if (y %in% c(2010:2014)) {
+    f<-files.hist[7]
+    file.id<-nc_histfiles[which(nc_histfiles$name==f),'id']
+  } else if (y %in% c(2015:2019)) {
+    f<-files.hist[8]
+    file.id<-nc_histfiles[which(nc_histfiles$name==f),'id']
+  } else if (y %in% c(2020)) {
+    f<-files.hist[9]
+    file.id<-nc_histfiles[which(nc_histfiles$name==f),'id']
+  } else if (y %in% c(2021:2022)) {
+    f<-files.for[1]
+    file.id<-nc_forfiles[which(nc_forfiles$name==f),'id']} #for year>2020 have to select projection
+  
+  #download file into temp folder
+  googledrive::drive_download(file=file.id$id,
+                              path = paste0('./Data/Bering 10K ROMS/',f),
+                              overwrite = TRUE)
+  
+  #open netcdf
+  nc<-nc_open(paste0('./Data/Bering 10K ROMS/',f))
+  
+  #dimensions netcdf files
+  #258 rows
+  #182 cols
+  #46956 cells
+  #259 time steps
+  
+  #get variables
+  #names(nc$var)
+  
+  #get latitude
+  lats <- ncvar_get(nc,"lat_rho")
+  
+  #get longitude
+  lons <- ncvar_get(nc,"lon_rho")
+  
+  #get SBT
+  temp<-ncvar_get(nc,'temp')
+  
+  #get time
+  t_axis<-ncvar_get(nc,"ocean_time")
+  
+  #convert time
+  time_axis <- as.POSIXct(t_axis, origin = "1900-01-01", tz = "GMT") 
+  
+  #get weekly temp slices from specific year y
+  nc_y<-ncvar_get(nc, "temp")[,,which(grepl(paste0(y,'-'),time_axis))]
+  
+  #get mean matrix for this year
+  mean_nc<-apply(nc_y,c(1,2),mean,na.rm=TRUE)
+  
+  #create dataframe with lats, lons and mean year SBT
+  df_nc<-data.frame(Lat=as.vector(lats),
+                    Lon=as.vector(lons),
+                    temp=as.vector(mean_nc))
+  
+  #longitude are in eastern. get SBT for the western hemisphere (Bering Sea). longitude greater than 180 degrees
+  df_nc1<-df_nc[which(df_nc$Lon>=180),]
+  
+  #convert eastern longitude to western values (higher). longitude should be negative
+  df_nc1$Lon<-df_nc1$Lon-180-180
+  
+  #filter values from the grid 
+  df_nc2<-subset(df_nc1,Lat >= min(grid.ebs$Lat) & Lat <= max(grid.ebs$Lat) & Lon >= min(grid.ebs$Lon) & Lon <= max(grid.ebs$Lon))
+  
+  #remove rows with NAs
+  df_nc3<-df_nc2[complete.cases(df_nc2),]
+  
+  #create spatial object from df
+  coordinates(df_nc3) <- ~ Lon + Lat
+  
+  #create spatial object from grid
+  spg <- grid.ebs
+  coordinates(spg) <- ~ Lon + Lat
+  
+  #get the nearests points from one df to other df
+  nn<-get.knnx(coordinates(df_nc3),coordinates(spg),1)
+  nc_index<-nn$nn.index[,1]
+  
+  #get SBT
+  temps<-as.data.frame(df_nc3)$temp[nc_index]
+  grid.ebs$BottomTempROMS<-temps
+  grid.ebs$Year<-y
+  
+  #incorporate SBT
+  grid.ebs_year<-rbind(grid.ebs_year,grid.ebs)
+  
+  #close netcdf file
+  nc_close(nc)
+  
+}
+
+#save grid Bering Sea with SBT and depth as dataframe
+saveRDS(grid.ebs_year,'./slope shelf EBS NBS VAST/grid_ebs_covariate_data.rds')
