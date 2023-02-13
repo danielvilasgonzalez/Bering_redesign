8cb48ef4758796d01ac0fec95e03f8f0b3c46659
Daniel Vilas <danielvilasgonzalez@gmail.com>
2023-02-13 10:37:43 -0800

    create R script file

---
+++ Scripts/05_fit_VAST_model.R
@@ -0,0 +1,31 @@
+####################################################################
+####################################################################
+##    
+##    fit single sp VAST models using depth and SBT
+##    Daniel Vilas (danielvilasgonzalez@gmail.com/dvilasg@uw.edu)
+##
+####################################################################
+####################################################################
+
+#clear all objects
+rm(list = ls(all.names = TRUE)) 
+#free up memrory and report the memory usage
+gc() 
+
+#libraries from cran to call or install/load
+pack_cran<-c('ncdf4','raster','FNN')
+
+#install pacman to use p_load function - call library and if not installed, then install
+if (!('pacman' %in% installed.packages())) {
+  install.packages("pacman")}
+
+#load/install packages
+pacman::p_load(pack_cran,character.only = TRUE)
+
+#setwd
+out_dir<-'E:/UW/Adapting Monitoring to a Changing Seascape/'
+setwd(out_dir)
+
+#range years of data
+sta_y<-2002
+end_y<-2022
