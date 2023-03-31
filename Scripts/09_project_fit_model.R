48ec6537be3c94d2ae222347491ef26998ff7336
Daniel Vilas <danielvilasgonzalez@gmail.com>
2023-03-31 14:58:22 -0700

    update spp and raster

--- Scripts/09_project_fit_model.R
+++ Scripts/09_project_fit_model.R
@@ -37,12 +37,27 @@ setwd(out_dir)
 #version VAST (cpp)
 version<-'VAST_v13_1_0'
 
-#sp
-sp<-'Gadus macrocephalus'
+#selected species
+spp<-c('Limanda aspera',
+       'Gadus chalcogrammus',
+       'Gadus macrocephalus',
+       'Atheresthes stomias',
+       'Reinhardtius hippoglossoides',
+       'Lepidopsetta polyxystra',
+       'Hippoglossoides elassodon',
+       'Pleuronectes quadrituberculatus',
+       'Hippoglossoides robustus',
+       'Boreogadus saida',
+       'Eleginus gracilis',
+       'Anoplopoma fimbria',
+       'Chionoecetes opilio',
+       'Paralithodes platypus',
+       'Paralithodes camtschaticus')
 
-##############################
+#############################
 #OBJECTS TO PLOT
 #############################
+
 # Define plot extent (through trial end error) units km
 panel_extent <- data.frame(x = c(-1716559.21, -77636.05), #x = c(-1326559.21, -87636.05),
                            y = c(483099.5, 2194909.7)) #y = c(533099.5, 1894909.7))
@@ -80,7 +95,6 @@ for (i in shfiles) {
     googledrive::drive_download(file=id.data$id[j],
                                 path = paste0('./shapefiles/',id.data$name[j]),
                                 overwrite = TRUE)
-    
   }
   
   #shapefile EBS
@@ -115,248 +129,263 @@ pal<-wesanderson::wes_palette('Zissou1',21,type='continuous')
 df_scn<-read.csv('./tables/SBT_scenarios.csv')
 df_scn<-df_scn[,1:8]
 
+#fit file
+ff<-'fit.RData'
+
+#create sp folder
+dir.create('./output/species/')
+
+#loop over species
+for (sp in spp) {
+
+  sp<-'Gadus macrocephalus'
+  
+  #create sp folder
+  dir.create(paste0('./output/species/',sp))
+  
 ##############################
-# FIT PROJECT SETTINGS
+# FIT PROJECT SETTINGS 
 #############################
 
-#fit file
-ff<-'b2_19822022fit.RData'
-
-#load fit file
-load(paste0('./shelf EBS NBS VAST/Gadus macrocephalus/temp3d/',ff))
-
-#add covariate data for the projected years into the fit$covariate_data
-fit$covariate_data
-
-#yrs
-yrs<-as.integer(fit$year_labels)
-
-#how manyt projected years we want
-n_proj<-5
-
-#project_yrs
-project_yrs<-(last(yrs)+1):(last(yrs)+n_proj)
+  #load fit file
+  load(paste0('./shelf EBS NBS VAST/',sp,'/',ff))
+  
+  #add covariate data for the projected years into the fit$covariate_data
+  #fit$covariate_data
+  
+  #yrs
+  yrs<-as.integer(fit$year_labels)
+  
+  #how manyt projected years we want
+  n_proj<-5
+  
+  #project_yrs
+  project_yrs<-(last(yrs)+1):(last(yrs)+n_proj)
 
 ##############################
 # PROJECT THROUGH SBT SCENARIOS
 #############################
 
-#get scenarios
-df_scn<-read.csv('./tables/SBT_scenarios.csv')
-df_scn<-df_scn[,c(1:8)]
+  #get scenarios
+  df_scn<-read.csv('./tables/SBT_scenarios.csv')
+  df_scn<-df_scn[,c(1:8)]
+  
+  #get raster stack
+  stack_files<-list.files('./data processed/SBT scenarios/')
+  
+  #list covariate data for each scenario
+  cov_list<-list()
+  
+  #list projected data for each scenario
+  pr_list<-list()
 
-#get raster stack
-stack_files<-list.files('./data processed/SBT scenarios/')
+    #loop over scenarios
+    for (scn in unique(df_scn$scn_n)) {
+      
+      #scn<-unique(df_scn$scn_n)[2]
+      
+      #print scenario to check progress
+      cat(paste(" #############     PROJECTING    #############\n",
+                " #############   Species", sp, match(sp,spp), 'out of',length(spp),  "  #############\n",
+                " #############  Scenario", scn, " #############\n"))
 
-#list covariate data for each scenario
-cov_list<-list()
-
-#list projected data for each scenario
-pr_list<-list()
-
-#loop over scenarios
-for (scn in unique(df_scn$scn_n)) {
-  
-  #scn<-unique(df_scn$scn_n)[2]
-  
-  #print scenario to check progress
-  cat(paste(" #############  Scenario", scn, " #############\n"))
-  
-  #load fit file
-  load(paste0('./shelf EBS NBS VAST/Gadus macrocephalus/temp3d/',ff))
-  
-  #open stack of rasters
-  st<-stack_files[grepl(paste0('scn',scn),stack_files)][1]
-  st<-stack(paste0('./data processed/SBT scenarios/',st))
-  
-  #reproject shapefile
-  proj4string(st) <- CRS('+proj=aea +lat_1=55 +lat_2=65 +lat_0=50 +lon_0=-154 +x_0=0 +y_0=0 +ellps=GRS80 +datum=NAD83 +units=m +no_defs') 
-  
-  #raster to points
-  points<-data.frame(rasterToPoints(st))
-  
-  #create a df to store
-  points3<-data.frame(matrix(nrow = 0,ncol = ncol(fit$covariate_data)))
-  names(points3)<-names(fit$covariate_data)
-  
-  for (y in project_yrs) {
+      #open stack of rasters
+      st<-stack_files[grepl(paste0('scn',scn),stack_files)][1]
+      st<-stack(paste0('./data processed/SBT scenarios/',st))
+      
+      #reproject shapefile
+      proj4string(st) <- CRS('+proj=aea +lat_1=55 +lat_2=65 +lat_0=50 +lon_0=-154 +x_0=0 +y_0=0 +ellps=GRS80 +datum=NAD83 +units=m +no_defs') 
+      
+      #raster to points
+      points<-data.frame(rasterToPoints(st))
+      
+      #create a df to store
+      points3<-data.frame(matrix(nrow = 0,ncol = ncol(fit$covariate_data)))
+      names(points3)<-names(fit$covariate_data)
+      
+      for (y in project_yrs) {
+        
+        #y<-project_yrs[1]
+        
+        #get points for year
+        points1<-points[,c('x','y',paste0('y',y))]
+        names(points1)<-c('Lon',"Lat",'BotTemp')
+        
+        #reproject df
+        coordinates(points1)<- ~ Lon + Lat
+        proj4string(points1) <- CRS('+proj=aea +lat_1=55 +lat_2=65 +lat_0=50 +lon_0=-154 +x_0=0 +y_0=0 +ellps=GRS80 +datum=NAD83 +units=m +no_defs') 
+        points1<-spTransform(points1,CRSobj = CRS("+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0"))
+        
+        #create a new df
+        points2<-data.frame(cbind(Year=y,Lat=points1$Lat,Lon=points1$Lon,ScaleLogDepth=NA,LogDepth=NA,ScaleBotTemp=NA,BotTemp=points1$BotTemp,CPUE_kg=NA))
+        
+        #add year
+        points3<-rbind(points3,points2)
+        
+      }
+      
+      
+      #add year to covariate data from fit
+      cov_list[[scn]]<-points3
+      
+      #add to covariate data
+      fit$covariate_data<-rbind(fit$covariate_data,points3)
     
-    #y<-project_yrs[1]
-    
-    #get points for year
-    points1<-points[,c('x','y',paste0('y',y))]
-    names(points1)<-c('Lon',"Lat",'BotTemp')
-    
-    #reproject df
-    coordinates(points1)<- ~ Lon + Lat
-    proj4string(points1) <- CRS('+proj=aea +lat_1=55 +lat_2=65 +lat_0=50 +lon_0=-154 +x_0=0 +y_0=0 +ellps=GRS80 +datum=NAD83 +units=m +no_defs') 
-    points1<-spTransform(points1,CRSobj = CRS("+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0"))
-    
-    #create a new df
-    points2<-data.frame(cbind(Year=y,Lat=points1$Lat,Lon=points1$Lon,ScaleLogDepth=NA,LogDepth=NA,ScaleBotTemp=NA,BotTemp=points1$BotTemp,CPUE_kg=NA))
-    
-    #add year
-    points3<-rbind(points3,points2)
-    
-  }
+      #project model example
+      pm<-project_model(x = fit,n_proj = n_proj,)
+      
+      #add year to covariate data from fit
+      pr_list[[paste0('scn',scn)]]<-pm
+    }
   
-  
-  #add year to covariate data from fit
-  cov_list[[scn]]<-points3
-  
-  #add to covariate data
-  fit$covariate_data<-rbind(fit$covariate_data,points3)
-
-  #project model example
-  pm<-project_model(x = fit,n_proj = n_proj)
-  
-  #add year to covariate data from fit
-  pr_list[[paste0('scn',scn)]]<-pm
+  #save projection list
+  save(pr_list, file = paste0("./output/species/",sp,'/fit_projection.RData'))
 }
-
-#save projection list
-save(pr_list, file = paste0("./output/",sp,'_projection_data.RData'))
 
 ##############################
 # PLOT PROJECTIONS
 #############################
 
-#loop
-for (proj in names(pr_list)) {
+#create sp folder
+dir.create('./figures/species/')
+
+#loop over species
+for (sp in spp) {
   
-  #proj<-names(pr_list)[1]
+  sp<-'Gadus macrocephalus'
   
-  #get object
-  p<-pr_list[[proj]]
+  #print scenario to check progress
+  cat(paste(" #############      PLOTTING     #############\n",
+            " #############   Species", sp, match(sp,spp), 'out of',length(spp),  "  #############\n",
+            " #############  Scenario", scn, " #############\n"))
   
-  #load fit file
-  load(paste0('./shelf EBS NBS VAST/Gadus macrocephalus/temp3d/',ff))
+  #create sp folder
+  dir.create(paste0('./figures/species/',sp))
   
-  #get predictions
-  D_gt<-p$D_gct[,1,]
-  #D_gt_proj<-D_gt[,paste0(project_yrs)]
-  D_gt<-drop_units(D_gt)
-  D_gt<-data.frame('cell'=c(1:fit$spatial_list$n_g),D_gt)
+  #save projection list
+  load(paste0("./output/species/",sp,'/fit_projection.RData')) #pr_list
   
-  colnames(D_gt)<-c('cell',c(fit$year_labels,project_yrs))
-  D_gt1<-reshape2::melt(D_gt,id=c('cell'))
-  
-  mdl <- make_map_info(Region = fit$settings$Region,
-                       spatial_list = fit$spatial_list,
-                       Extrapolation_List = fit$extrapolation_list)
-  
-  D <- merge(D_gt1, mdl$PlotDF, by.x='cell', by.y='x2i')
-  
-  # #extract with extrapolation mesh
-  # mesh<-fit$spatial_list
-  # class(fit$spatial_list)
-  # plot(mesh$MeshList)
-  # mesh<-fit$spatial_list
-  # mesh1<-mesh$MeshList$anisotropic_mesh
-  # class(mesh1)
-  # 
-  # #looking at the spatial field and what it looks like
-  # gproj <- inla.mesh.projector(mesh,  dims = c(300, 300))
-  # g.mean <- inla.mesh.project(gproj, model1$summary.random$spatial.field$mean)
-  # g.sd <- inla.mesh.project(gproj, model1$summary.random$spatial.field$sd)
-  # 
-  #plot list to store plots
-  plot_list<-list()
-  
-  #loop over years
-  for (y in project_yrs) {
+  for (proj in names(pr_list)) {
     
-    #y<-project_yrs[1]
+    #proj<-names(pr_list)[1]
     
-    #subset by year
-    D1<-subset(D,variable==y)
-    D2<-D1[,c("value","Lat","Lon")]
+    #get object
+    p<-pr_list[[proj]]
     
-    #df to spatialpoint df
-    coordinates(D2) <- ~ Lon + Lat
-    crs(D2)<-c(crs="+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0")
+    #load fit file
+    load(paste0('./shelf EBS NBS VAST/',sp,'/',ff))
     
-    #reproject coordinates for plotting purposes
-    D2_1<-spTransform(D2,'+proj=aea +lat_1=55 +lat_2=65 +lat_0=50 +lon_0=-154 +x_0=0 +y_0=0 +ellps=GRS80 +datum=NAD83 +units=m +no_defs')
-    D2_2<-data.frame(D2_1)
+    #get predictions
+    D_gt<-p$D_gct[,1,]
+    #D_gt_proj<-D_gt[,paste0(project_yrs)]
+    D_gt<-drop_units(D_gt)
+    D_gt<-data.frame('cell'=c(1:fit$spatial_list$n_g),D_gt)
     
-    #x and y cells
-    xycells<-as.integer(sqrt(dim(D2_1)[1]))
+    colnames(D_gt)<-c('cell',c(fit$year_labels,project_yrs))
+    D_gt1<-reshape2::melt(D_gt,id=c('cell'))
     
-    # create a template raster
-    r1 <- raster(ext=extent(D2_1),ncol=xycells, nrow=xycells) #c(15800,15800) 7000
+    mdl <- make_map_info(Region = fit$settings$Region,
+                         spatial_list = fit$spatial_list,
+                         Extrapolation_List = fit$extrapolation_list)
     
-    #create raster
-    r2<-rasterize(D2_1, r1 ,field='value')
-    crs(r2) <- CRS('+proj=aea +lat_1=55 +lat_2=65 +lat_0=50 +lon_0=-154 +x_0=0 +y_0=0 +ellps=GRS80 +datum=NAD83 +units=m +no_defs')
-    #plot(r2)
-   
-    #plot density map
-    p<-
-      ggplot() +
-        geom_raster(data=as.data.frame(r2, xy = TRUE),aes(x=x,y=y,fill=log(layer)))+
+    D <- merge(D_gt1, mdl$PlotDF, by.x='cell', by.y='x2i')
+    
+    #plot list to store plots
+    plot_list<-list()
+    
+    #loop over years
+    for (y in project_yrs) {
       
-      #geom_point(data=D2_2, aes(Lon, Lat, color=log(as.vector(value)), group=NULL),
-                 ## These settings are necessary to avoid
-                 ## overlplotting which is a problem here. May need
-                 ## to be tweaked further.
-                 #ize=1.2, stroke=0,shape=16)+ #tune size to remove blanks on the map, depending on the resolution of the tiff
-      geom_polygon(data=ak_sppoly,aes(x=long,y=lat,group=group),fill = 'grey60')+
-      scale_x_continuous(expand = c(0,0),
-                         breaks = c(-175,-170,-165,-160))+
-      scale_y_continuous(expand = c(0,0),
-                         breaks = c(65,60,55))+
-      geom_polygon(data=nbs_sh,aes(x=long,y=lat,group=group),fill=NA,col='black')+
-      geom_polygon(data=ebs_sh,aes(x=long,y=lat,group=group),fill=NA,col='black')+
-      coord_sf(crs = '+proj=aea +lat_1=55 +lat_2=65 +lat_0=50 +lon_0=-154 +x_0=0 +y_0=0 +ellps=GRS80 +datum=NAD83 +units=m +no_defs',
-               xlim = c(-1486559.21, -77636.05),
-               ylim = c(453099.5,2004909.7),
-               label_axes = "-NE-")+
-      scale_fill_gradientn(colours = pal,name=('log(kg/km²)'),na.value = 'white',
-                            guide = guide_colorbar(  frame.colour = "black",ticks.colour = 'black'))+
-      #annotation_north_arrow(location = "tr", which_north = "true",pad_x = unit(0.01, 'points'), pad_y = unit(10, 'points'),
-      #                       style = north_arrow_fancy_orienteering(line_width = 1.5, text_size =6))+
-      theme(aspect.ratio = 1,panel.grid.major = element_line(color = rgb(0, 0, 0,20, maxColorValue = 285), linetype = 'dashed', linewidth =  0.5),
-            panel.background = element_rect(fill = NA),panel.ontop = TRUE,text = element_text(size=10),
-            legend.background =  element_rect(fill = "transparent", colour = "transparent"),legend.key.height= unit(10, 'points'),
-            legend.key.width= unit(10, 'points'),axis.title = element_blank(),legend.position = c(0.12,0.23),
-            panel.border = element_rect(fill = NA, colour = 'black'),legend.key = element_rect(color="black"),
-            axis.text = element_text(color='black'),legend.spacing.y = unit(8, 'points'),
-            axis.text.y.right = element_text(hjust= 0.1 ,margin = margin(-7,0,0,-25, unit = 'points'),color='black'),
-            axis.text.x = element_text(vjust = 6, margin = margin(-7,0,0,-25, unit = 'points'),color='black'),
-            axis.ticks.length = unit(-5,"points"),plot.title = element_text(size=12,vjust = -10, hjust=0.95,face="bold"),
-            plot.margin=margin(c(5,5,5,5)),legend.title = element_text(size = 8))+
-      #annotate("text", x = -256559, y = 1354909, label = "Alaska",parse=TRUE,size=7)+
-      #annotate("text", x = -1176559, y = 1904909, label = "Russia",parse=TRUE,size=7)+
-      labs(title=paste0(y))
+      #y<-project_yrs[1]
+      
+      #subset by year
+      D1<-subset(D,variable==y)
+      D2<-D1[,c("value","Lat","Lon")]
+      
+      #df to spatialpoint df
+      coordinates(D2) <- ~ Lon + Lat
+      crs(D2)<-c(crs="+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0")
+      
+      #reproject coordinates for plotting purposes
+      D2_1<-spTransform(D2,'+proj=aea +lat_1=55 +lat_2=65 +lat_0=50 +lon_0=-154 +x_0=0 +y_0=0 +ellps=GRS80 +datum=NAD83 +units=m +no_defs')
+      D2_2<-data.frame(D2_1)
+      
+      #x and y cells
+      xycells<-as.integer(sqrt(dim(D2_1)[1]))
+      
+      # create a template raster
+      r1 <- raster(ext=extent(D2_1),ncol=xycells, nrow=xycells) #c(15800,15800) 7000
+      
+      #create raster
+      r2<-rasterize(D2_1, r1 ,field='value')
+      crs(r2) <- CRS('+proj=aea +lat_1=55 +lat_2=65 +lat_0=50 +lon_0=-154 +x_0=0 +y_0=0 +ellps=GRS80 +datum=NAD83 +units=m +no_defs')
+      #plot(r2)
+     
+      #plot density map
+      p<-
+        ggplot() +
+        geom_raster(data=as.data.frame(r2, xy = TRUE),aes(x=x,y=y,fill=log(layer)))+
+        #geom_point(data=D2_2, aes(Lon, Lat, color=log(as.vector(value)), group=NULL),
+                   ## These settings are necessary to avoid
+                   ## overlplotting which is a problem here. May need
+                   ## to be tweaked further.
+                   #ize=1.2, stroke=0,shape=16)+ #tune size to remove blanks on the map, depending on the resolution of the tiff
+        geom_polygon(data=ak_sppoly,aes(x=long,y=lat,group=group),fill = 'grey60')+
+        scale_x_continuous(expand = c(0,0),
+                           breaks = c(-175,-170,-165,-160))+
+        scale_y_continuous(expand = c(0,0),
+                           breaks = c(65,60,55))+
+        geom_polygon(data=nbs_sh,aes(x=long,y=lat,group=group),fill=NA,col='black')+
+        geom_polygon(data=ebs_sh,aes(x=long,y=lat,group=group),fill=NA,col='black')+
+        coord_sf(crs = '+proj=aea +lat_1=55 +lat_2=65 +lat_0=50 +lon_0=-154 +x_0=0 +y_0=0 +ellps=GRS80 +datum=NAD83 +units=m +no_defs',
+                 xlim = c(-1486559.21, -77636.05),
+                 ylim = c(453099.5,2004909.7),
+                 label_axes = "-NE-")+
+        scale_fill_gradientn(colours = pal,name=('log(kg/km²)'),na.value = 'white',
+                              guide = guide_colorbar(  frame.colour = "black",ticks.colour = 'black'))+
+        #annotation_north_arrow(location = "tr", which_north = "true",pad_x = unit(0.01, 'points'), pad_y = unit(10, 'points'),
+        #                       style = north_arrow_fancy_orienteering(line_width = 1.5, text_size =6))+
+        theme(aspect.ratio = 1,panel.grid.major = element_line(color = rgb(0, 0, 0,20, maxColorValue = 285), linetype = 'dashed', linewidth =  0.5),
+              panel.background = element_rect(fill = NA),panel.ontop = TRUE,text = element_text(size=10),
+              legend.background =  element_rect(fill = "transparent", colour = "transparent"),legend.key.height= unit(10, 'points'),
+              legend.key.width= unit(10, 'points'),axis.title = element_blank(),legend.position = c(0.12,0.23),
+              panel.border = element_rect(fill = NA, colour = 'black'),legend.key = element_rect(color="black"),
+              axis.text = element_text(color='black'),legend.spacing.y = unit(8, 'points'),
+              axis.text.y.right = element_text(hjust= 0.1 ,margin = margin(-7,0,0,-25, unit = 'points'),color='black'),
+              axis.text.x = element_text(vjust = 6, margin = margin(-7,0,0,-25, unit = 'points'),color='black'),
+              axis.ticks.length = unit(-5,"points"),plot.title = element_text(size=12,vjust = -10, hjust=0.95,face="bold"),
+              plot.margin=margin(c(5,5,5,5)),legend.title = element_text(size = 8))+
+        #annotate("text", x = -256559, y = 1354909, label = "Alaska",parse=TRUE,size=7)+
+        #annotate("text", x = -1176559, y = 1904909, label = "Russia",parse=TRUE,size=7)+
+        labs(title=paste0(y))
+      
+      plot_list[[as.character(y)]]<-p
+      
+    }
     
-    plot_list[[as.character(y)]]<-p
+    #scenario
+    scn<-gsub('scn','',proj)
     
-  }
-  
-  #scenario
-  scn<-gsub('scn','',proj)
-  
-  #scenario name
-  scn_name<-df_scn[which(df_scn$scn_n==scn),'Scenario']
-  
-  # now add the title
-  title <- ggdraw() + 
-    draw_label(
-      paste0("Scn",scn,' - ',scn_name),
-      fontface = 'bold',hjust=0.5,size=16,vjust=0.7
-    ) 
-  
-  #save multiplot
-  mp<-cowplot::plot_grid(plotlist = plot_list,nrow = 1,ncol = n_proj)
-  ragg::agg_png(paste0('./figures/projection_',sp,'_',proj,".png"), width = 20, height = 4.6, units = "in", res = 300)
-  print(
-      plot_grid(
-      title, mp,
-      ncol = 1,
-      # rel_heights values control vertical title margins
-      rel_heights = c(0.08, 1)),align = "hv")
-  dev.off()
-  
+    #scenario name
+    scn_name<-df_scn[which(df_scn$scn_n==scn),'Scenario']
+    
+    # now add the title
+    title <- ggdraw() + 
+      draw_label(
+        paste0("Scn",scn,' - ',scn_name),
+        fontface = 'bold',hjust=0.5,size=16,vjust=0.7
+      ) 
+    
+    #save multiplot
+    mp<-cowplot::plot_grid(plotlist = plot_list,nrow = 1,ncol = n_proj)
+    ragg::agg_png(paste0('./figures/species/sp/projection_',proj,".png"), width = 20, height = 4.6, units = "in", res = 300)
+    print(
+        plot_grid(
+        title, mp,
+        ncol = 1,
+        # rel_heights values control vertical title margins
+        rel_heights = c(0.08, 1)),align = "hv")
+    dev.off()
+    
+  }  
 }  
-  
