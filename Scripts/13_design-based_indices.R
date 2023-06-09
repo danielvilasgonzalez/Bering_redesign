872f05de584c01c6a43f53ca325e4e93a846fdd2
Daniel Vilas <danielvilasgonzalez@gmail.com>
2023-06-09 11:11:24 -0700

    add extra approach

--- Scripts/13_design-based_indices.R
+++ Scripts/13_design-based_indices.R
@@ -10,6 +10,10 @@
 ####################################################################
 ####################################################################
 
+######################
+# SETTINGS
+######################
+
 #clear all objects
 rm(list = ls(all.names = TRUE)) 
 #free up memrory and report the memory usage
@@ -75,8 +79,9 @@ for (sp in spp) {
   
   #get samples scenarios
   lf<-list.files(path = paste0('./output/species/',sp,'/survey simulation historical data/0001/'),pattern = 'sim_survey')
-  lf1<-gsub('sim_survey','',lf)
-  lf2<-gsub('.RData','',lf1)
+  lf<-lf[grep('_dynamic',lf)]
+  lf1<-gsub('sim_survey_','',lf)
+  lf2<-gsub('_dynamic.RData','',lf1)
   
   #folder simulations
   ld<-list.dirs(path = paste0('./output/species/',sp,'/survey simulation historical data/'),full.names = FALSE,recursive = FALSE)
@@ -86,44 +91,37 @@ for (sp in spp) {
 
   #array to store
   index_hist<-array(NA,
-                     dim=list(100,4,41,length(ld),length(lf2)),
-                     dimnames=list(1:100,c('STRS_mean','STRS_var','cv','index'),paste0('y',1982:2022),ld,lf2))
+                     dim=list(4,41,3,length(ld),length(lf2)),
+                     dimnames=list(c('STRS_mean','STRS_var','cv','index'),paste0('y',1982:2022),c('current','buffer','random'),ld,lf2))
   
   
-  #loop over simulation
+  #loop over simulation (n_sim=100)
   for (sim in ld) {
     
     #sim<-ld[1]
    
-    #print scenario to check progress
-    #cat(paste(" #############  ", sim, " #############\n"))
-    
     #loop over sampling designs
     for (samp in lf2) {
       
-      #samp<-lf2[49]
+       #samp<-lf2[1]
+       #samp<-lf2[1]
       
       #print scenario to check progress
       cat(paste(" #############  ", sim, " #############\n",
                 " #############  ", samp, " #############\n"))
       
       #load survey samples from historical
-      load( file = paste0("./output/species/",sp,'/survey simulation historical data/',sim,'/sim_survey',samp,'.RData')) #sim_survey
+      load( file = paste0("./output/species/",sp,'/survey simulation historical data/',sim,'/sim_survey_',samp,'_dynamic.RData')) #sim_survey
       
-      #when base scenario there is no replicates
+      #when base sampling other files
       if (grepl('base',samp)) {
         
-        #replicates
-        repls<-1
-        
-        #in case scn base need to remove stations
         #conditions on baseline scenarios
         if (samp == 'scnbase') {
           baseline_strata$locations2<-baseline_strata$locations
         } else if (samp == 'scnbase_bis') {
           baseline_strata$locations2<-baseline_strata$locations[which(baseline_strata$locations$corner=='FALSE'),]
         } 
-        
         
         #sort by strata
         baseline_strata$strata_areas<-baseline_strata$strata_areas[order(baseline_strata$strata_areas$X1),]
@@ -140,11 +138,8 @@ for (sp in spp) {
         #weight of strata for each
         survey_detail$Wh <- survey_detail$Nh / sum(survey_detail$Nh)
         survey_detail$wh <- with(survey_detail, nh/Nh)
-        
+      
       } else {
-        
-        #replicates
-        repls<-1:100
         
         #load optimization results
         load(paste0('./output/species/',sp,'/optimization data/optimization_results_',samp,'.RData')) #result_list
@@ -165,23 +160,18 @@ for (sp in spp) {
         #weight of strata for each
         survey_detail$Wh <- survey_detail$Nh / sum(survey_detail$Nh)
         survey_detail$wh <- with(survey_detail, nh/Nh)
-        
-        #subset by sampling design
-        sum_stats<-subset(sp_sum_stats,samp_scn==samp)
-        
-      }  
       
-      #loop over replicates
-      for (repl in repls) { #:100
-        
-        #repl<-1
+      }   
+      
+      #loop over approaches  
+      for (apr in dimnames(sim_survey)[[3]]) {
         
         #get strata mean over year  
-        dens<-unlist(sim_survey[,,repl])
+        dens<-unlist(sim_survey[,,apr])
         y<-data.frame(dens)
         yy<-reshape2::melt(y,id.vars=c('strata'))
         
-        #mean
+        #mean by strata and year (variable)
         yyy<-aggregate(x=yy$value,
                        by=list(strata=yy$strata,year=yy$variable),
                        FUN = function(x) c(mean = mean(x,na.rm=T), var = var(x,na.rm=T) ))
@@ -190,39 +180,36 @@ for (sp in spp) {
         yyy$year<-gsub('X','',yyy$year) 
         yyyy<-do.call(data.frame, yyy)
         names(yyyy)<-c('strata','year','mean','var')
-        
+          
         #merge weight and strata
         yyyyy<-merge(yyyy,survey_detail[,c("Stratum","Wh")],by.x='strata',by.y='Stratum',all.x=TRUE)
         yyyyy<-merge(yyyyy,strata_areas,by.x='strata',by.y='X1',all.x=TRUE)
         yyyyyy<-merge(yyyyy,data.frame('strata'=unique(survey_detail$Stratum),'v'=with(survey_detail, Wh^2 * (1 - wh) / nh)),by='strata',all.x=TRUE)
-        
+          
         #get average mean
         yyyyyy$mean_w<-yyyyyy$mean*yyyyyy$Wh
         yyyyyy$index<-yyyyyy$mean*yyyyyy$Area_in_survey_km2
         yyyyyy$var_w<-yyyyyy$var*yyyyyy$v
-        
+          
         #area by strata
         yyyyyyy <- aggregate(yyyyyy[,c('mean_w','var_w','index')], by= list(yyyyyy$year) , 
                              FUN = sum)
-        
+          
         #get outputs
         STRS_mean <- yyyyyyy$mean_w
         STRS_var <- yyyyyyy$var_w
         CV <- sqrt(STRS_var) / STRS_mean
         index<- yyyyyyy$index/1000
-        
-        index_hist[repl,'STRS_mean',,sim,samp]<-STRS_mean
-        index_hist[repl,'STRS_var',,sim,samp]<-STRS_var
-        index_hist[repl,'cv',,sim,samp]<-CV
-        index_hist[repl,'index',,sim,samp]<-index
-        
-        }
+          
+        index_hist['STRS_mean',,apr,sim,samp]<-STRS_mean
+        index_hist['STRS_var',,apr,sim,samp]<-STRS_var
+        index_hist['cv',,apr,sim,samp]<-CV
+        index_hist['index',,apr,sim,samp]<-index
+      } 
      }
    }
-    
    #save object
    save(index_hist, file = paste0("./output/species/",sp,'/historical design-based indices/indices.RData'))
-  
 }
 
 ######################
@@ -239,9 +226,11 @@ for (sp in spp) {
   
   #get samples scenarios
   lf<-list.files(path = paste0('./output/species/',sp,'/survey simulation projected data/0001/'),pattern = 'sim_survey')
+  lf<-lf[grepl('_dynamic',lf)]
   lf0<-lf[grepl('SBT',lf)]
   lf1<-gsub('sim_survey_','',lf0)
   lf2<-unique(sub("\\_.*", "", lf1))
+  lf2<-c('scn1','scn2','scn3','scnbase','scnbase_bis')
   
   #folder simulations
   ld<-list.dirs(path = paste0('./output/species/',sp,'/survey simulation projected data/'),full.names = FALSE,recursive = FALSE)
@@ -251,17 +240,14 @@ for (sp in spp) {
 
   #array to store
   index_proj<-array(NA,
-                    dim=list(100,4,5,length(ld),length(lf2),length(paste0('SBT',1:12))),
-                    dimnames=list(1:100,c('STRS_mean','STRS_var','cv','index'),paste0('y',2023:2027),ld,lf2,paste0('SBT',1:12)))
+                    dim=list(4,5,3,length(ld),length(lf2),length(paste0('SBT',1:12))),
+                    dimnames=list(c('STRS_mean','STRS_var','cv','index'),paste0('y',2023:2027),c('current','buffer','random'),ld,lf2,paste0('SBT',1:12)))
   
   #loop over simulation
   for (sim in ld) {
     
     #sim<-ld[1]
-    
-    #print scenario to check progress
-    #cat(paste(" #############  ", sim, " #############\n"))
-    
+
     #loop over sbt scenarios
     for (sbt in paste0('SBT',1:12)) {
     
@@ -270,7 +256,8 @@ for (sp in spp) {
       #loop over sampling designs
       for (samp in lf2) {
         
-        #samp<-lf2[48]
+        #samp<-lf2[1]
+        #samp<-lf2[5]
         
         #print scenario to check progress
         cat(paste(" #############  ", sim, " #############\n",
@@ -278,20 +265,17 @@ for (sp in spp) {
                   " #############  ", samp, " #############\n"))
         
         #load survey samples from historical
-        load( file = paste0("./output/species/",sp,'/survey simulation projected data/',sim,'/sim_survey_',samp,'_',sbt,'.RData')) #sim_survey
+        load( file = paste0("./output/species/",sp,'/survey simulation projected data/',sim,'/sim_survey_',samp,'_',sbt,'_dynamic.RData')) #sim_survey
 
-        #when base scenario there is no replicates
+        #when base sampling other files
         if (grepl('base',samp)) {
           
-          #replicates
-          repls<-1
-          
-          if (samp == "scnbase") {
+          #conditions on baseline scenarios
+          if (samp == 'scnbase') {
             baseline_strata$locations2<-baseline_strata$locations
-          } else if (samp == "scnbase25") { #remove 25 stations
-            baseline_strata$locations2<-baseline_strata$locations[which(baseline_strata$locations$rm25==1),]
-          } else if (samp == "scnbase9") { #remove 9 stations
-            baseline_strata$locations2<-baseline_strata$locations[which(baseline_strata$locations$rm9==1),] }
+          } else if (samp == 'scnbase_bis') {
+            baseline_strata$locations2<-baseline_strata$locations[which(baseline_strata$locations$corner=='FALSE'),]
+          } 
           
           #sort by strata
           baseline_strata$strata_areas<-baseline_strata$strata_areas[order(baseline_strata$strata_areas$X1),]
@@ -310,9 +294,6 @@ for (sp in spp) {
           survey_detail$wh <- with(survey_detail, nh/Nh)
           
         } else {
-          
-          #replicates
-          repls<-1:100
           
           #load optimization results
           load(paste0('./output/species/',sp,'/optimization data/optimization_results_',samp,'.RData')) #result_list
@@ -333,19 +314,13 @@ for (sp in spp) {
           #weight of strata for each
           survey_detail$Wh <- survey_detail$Nh / sum(survey_detail$Nh)
           survey_detail$wh <- with(survey_detail, nh/Nh)
-          
-          #subset by sampling design
-          sum_stats<-subset(sp_sum_stats,samp_scn==samp)
-          
         }  
         
-        #loop over replicates
-        for (repl in repls) { #:100
-          
-          #repl<-1
-          
+        #loop over approaches
+        for (apr in dimnames(sim_survey)[[3]]) {
+        
           #get strata mean over year  
-          dens<-unlist(sim_survey[,,repl])
+          dens<-unlist(sim_survey[,,apr])
           y<-data.frame(dens)
           yy<-reshape2::melt(y,id.vars=c('strata'))
           
@@ -353,17 +328,17 @@ for (sp in spp) {
           yyy<-aggregate(x=yy$value,
                          by=list(strata=yy$strata,year=yy$variable),
                          FUN = function(x) c(mean = mean(x,na.rm=T), var = var(x,na.rm=T) ))
-          
+            
           #rename and dataframe
           yyy$year<-gsub('X','',yyy$year) 
           yyyy<-do.call(data.frame, yyy)
           names(yyyy)<-c('strata','year','mean','var')
-          
+            
           #merge weight and strata
           yyyyy<-merge(yyyy,survey_detail[,c("Stratum","Wh")],by.x='strata',by.y='Stratum',all.x=TRUE)
           yyyyy<-merge(yyyyy,strata_areas,by.x='strata',by.y='X1',all.x=TRUE)
           yyyyyy<-merge(yyyyy,data.frame('strata'=unique(survey_detail$Stratum),'v'=with(survey_detail, Wh^2 * (1 - wh) / nh)),by='strata',all.x=TRUE)
-          
+            
           #get average mean
           yyyyyy$mean_w<-yyyyyy$mean*yyyyyy$Wh
           yyyyyy$index<-yyyyyy$mean*yyyyyy$Area_in_survey_km2
@@ -371,8 +346,8 @@ for (sp in spp) {
           
           #area by strata
           yyyyyyy <- aggregate(yyyyyy[,c('mean_w','var_w','index')], by= list(yyyyyy$year) , 
-                              FUN = sum)
-
+                               FUN = sum)
+            
           #get outputs
           STRS_mean <- yyyyyyy$mean_w
           STRS_var <- yyyyyyy$var_w
@@ -380,16 +355,15 @@ for (sp in spp) {
           index<- yyyyyyy$index/1000
           
           #append results
-          index_proj[repl,'STRS_mean',,sim,samp,sbt]<-STRS_mean
-          index_proj[repl,'STRS_var',,sim,samp,sbt]<-STRS_var
-          index_proj[repl,'cv',,sim,samp,sbt]<-CV
-          index_proj[repl,'index',,sim,samp,sbt]<-index
-      
-            
-          }
-        }
+          index_proj['STRS_mean',,apr,sim,samp,sbt]<-STRS_mean
+          index_proj['STRS_var',,apr,sim,samp,sbt]<-STRS_var
+          index_proj['cv',,apr,sim,samp,sbt]<-CV
+          index_proj['index',,apr,sim,samp,sbt]<-index
+           
+        }  
       }
     }
+  }
   
   #save object
   save(index_proj, file = paste0("./output/species/",sp,'/projected design-based indices/indices.RData'))
