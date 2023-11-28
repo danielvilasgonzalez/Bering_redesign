64d61af188e04b615c715ac0aa591924e87a48b4
Daniel Vilas <danielvilasgonzalez@gmail.com>
2023-11-28 13:29:43 -0800

    check strata

--- Scripts/last version/crab_shapefiles.R
+++ Scripts/last version/crab_shapefiles.R
@@ -20,11 +20,18 @@ gdb_table5 <- st_read(dsn = gdb_path, layer = gdb_layers$name[5])
 gdb_table6 <- st_read(dsn = gdb_path, layer = gdb_layers$name[6])
 
 # View the structure and summary of the data
-str(gdb_table)
-summary(gdb_table)
-plot(gdb_table)
+str(gdb_table1)
+summary(gdb_table1)
+plot(gdb_table1)
 
 gdb_layers$name
+
+for (a in gdb_layers$name) {
+  
+  
+  
+  
+}
 
 
 ####################################################################
@@ -66,31 +73,31 @@ panel_extent <- data.frame(x = c(-1716559.21, -77636.05), #x = c(-1326559.21, -8
                            y = c(483099.5, 2194909.7)) #y = c(533099.5, 1894909.7))
 
 #Alaska layer
-ebs_layers <- akgfmaps::get_base_layers(select.region = "ebs", set.crs = "EPSG:3338")
+ebs_layers <- akgfmaps::get_base_layers(select.region = "ebs", set.crs = "+proj=longlat +datum=WGS84 +no_defs")
 ak_sppoly<-as(ebs_layers$akland, 'Spatial')
 
 
 
 gdb_layers$name
 
-
+gdb_table1
 
 
 ggplot()+
-  #geom_sf(data=ebs_layers$survey.strata,fill = NA)+ gdb_table
-  geom_sf(data=gdb_table1,fill = 'green',alpha=0.1)+ 
-  geom_sf_text(data = gdb_table1, label=gdb_layers$name[1],color='green')+
-  geom_sf(data=gdb_table2,fill = 'red',alpha=0.1)+ 
-  geom_sf_text(data = gdb_table2, label=gdb_layers$name[2],color='red')+
-  geom_sf(data=gdb_table3,fill = 'yellow',alpha=0.1)+ 
-  geom_sf_text(data = gdb_table3, label=gdb_layers$name[3],color='yellow')+
-  geom_sf(data=gdb_table4,fill = 'blue',alpha=0.1)+ 
-  geom_sf_text(data = gdb_table4, label=gdb_layers$name[4],color='blue')+
-  geom_sf(data=gdb_table5,fill = 'orange',alpha=0.1)+ 
-  geom_sf_text(data = gdb_table5, label=gdb_layers$name[5],color='orange')+
-  geom_sf(data=gdb_table6,fill = 'black',alpha=0.1)+ 
+  geom_sf(data=ebs_layers$survey.strata,fill = NA)+ #gdb_table
+  # geom_sf(data=gdb_table1,fill = 'green',alpha=0.1)+
+  # geom_sf_text(data = gdb_table1, label=gdb_layers$name[1],color='green')+
+  # # geom_sf(data=gdb_table2,fill = 'red',alpha=0.1)+
+  # # geom_sf_text(data = gdb_table2, label=gdb_layers$name[2],color='red')+
+  # geom_sf(data=gdb_table3,fill = 'yellow',alpha=0.1)+
+  # geom_sf_text(data = gdb_table3, label=gdb_layers$name[3],color='yellow')+
+  # # geom_sf(data=gdb_table4,fill = 'blue',alpha=0.1)+
+  # # geom_sf_text(data = gdb_table4, label=gdb_layers$name[4],color='blue')+
+  # geom_sf(data=gdb_table5,fill = 'orange',alpha=0.1)+
+  # geom_sf_text(data = gdb_table5, label=gdb_layers$name[5],color='orange')+
+  geom_sf(data=gdb_table6,fill = 'black',alpha=0.1)+
   geom_sf_text(data = gdb_table6, label=gdb_layers$name[6],color='black')+
-  geom_polygon(data=ak_sppoly,aes(x=long,y=lat,group=group),fill = 'grey60')+
+  #geom_polygon(data=ak_sppoly,aes(x=long,y=lat,group=group),fill = 'grey60')+
   scale_x_continuous(expand = c(0,0),position = 'bottom',
                      breaks = c(-175,-170,-165,-160,-155),sec.axis = dup_axis())+
   #geom_polygon(data=NBS_sh,aes(x=long,y=lat,group=group),fill=NA,col='black')+
@@ -110,3 +117,67 @@ ggplot()+
         axis.text.x = element_text(vjust = 6, margin = margin(-7,0,7,0, unit = 'points'),color='black'),
         axis.ticks.length = unit(-5,"points"))+
   scale_y_continuous(expand = c(0,0),position = 'right',sec.axis = dup_axis())#+
+
+
+########################
+# stratification crabs from LDF
+#######################
+
+crab_spp<-c('bairdi','bkc','opilio','rkc')
+
+for (sp in crab_spp) {
+  
+strata<-read.csv(paste0('/Users/daniel/Documents/GitHub/Corner-Stations-II/strata_',sp,'_newts_baseline.csv'))
+strata$STRATUM<-as.factor(strata$STRATUM)
+# strata20<-data.frame('X',NA,NA,'2020',mean(strata$LATITUDE),mean(strata$LONGITUDE),NA,0)
+# names(strata20)<-names(strata)
+# strata<-rbind(strata,strata20)
+# strata$SURVEY_YEAR<-as.factor(strata$SURVEY_YEAR)              
+# levels(strata$SURVEY_YEAR)<-c(levels(strata$SURVEY_YEAR),c('2020'))
+ebs_layers$survey.strata<-spTransform(ebs_layers$survey.strata,CRSobj = "+proj=longlat +datum=WGS84 +no_defs")
+
+tail(strata)
+print(
+ggplot()+
+  geom_sf(data=ebs_layers$survey.strata,fill = NA)+
+  geom_point(data=subset(strata,SURVEY_YEAR %in% c(1992,2021)),aes(x=LONGITUDE,y=LATITUDE,color=STRATUM),size=0.5)+
+  facet_wrap(~SURVEY_YEAR)+
+  xlim(c(-180,-155))+
+  ylim(c(50,67))+
+  # coord_sf(crs = '+proj=aea +lat_1=55 +lat_2=65 +lat_0=50 +lon_0=-154 +x_0=0 +y_0=0 +ellps=GRS80 +datum=NAD83 +units=m +no_defs',
+  #          #xlim = c(panel_extent$x[1]+200000,panel_extent$x[2]),
+  #          xlim = c(panel_extent$x[1]+200000,panel_extent$x[2]+100000),
+  #          ylim = c(panel_extent$y[1]-100000,panel_extent$y[2]-200000),
+  #          label_axes = "-NE-")+
+  ggtitle(sp)
+)
+}
+
+yr<-unique(strata$SURVEY_YEAR)
+sort(as.numeric(names(which(table(strata$STRATUM[strata$SURVEY_YEAR==yr]) > 1))))
+
+
+
+for(y in 1:n_year){
+  #need to ensure that at least two stations were sampled for a strata to be included in a given year for sd calculations
+  strata_yr_list[[i]][[y]] <- sort(as.numeric(names(which(table(strata$STRATUM[strata$SURVEY_YEAR==yr[y]]) > 1))))
+  station_strata_yr_list[[i]][[y]] <- replicate(length(strata_yr_list[[i]][[y]]), vector())
+  for(g in 1:length(strata_yr_list[[i]][[y]])){
+    strata_area_list[[i]][[y]][g] <- unique(strata$TOTAL_AREA[strata$SURVEY_YEAR==yr[y] & strata$STRATUM==strata_yr_list[[i]][[y]][g]])*3.4299
+    station_strata_yr_list[[i]][[y]][[g]] <- strata$STATION_ID[strata$SURVEY_YEAR==yr[y] & strata$STRATUM==strata_yr_list[[i]][[y]][g]]
+    #print(length(station_strata_yr_list[[i]][[y]][[g]]))
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
