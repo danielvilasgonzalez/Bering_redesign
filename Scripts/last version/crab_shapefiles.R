c0af94e0fb4005d613a49ebcd516d5f34044c36a
Daniel Vilas <danielvilasgonzalez@gmail.com>
2023-12-20 10:39:00 -0800

    crab shapefile plot

--- Scripts/last version/crab_shapefiles.R
+++ Scripts/last version/crab_shapefiles.R
@@ -1,4 +1,5 @@
 library(sf)
+library(sp)
 
 # Set the path to the geodatabase file
 gdb_path <- "./shapefiles/CrabStrataShapefiles_GAPgrid.gdb/"
@@ -21,18 +22,174 @@ gdb_table6 <- st_read(dsn = gdb_path, layer = gdb_layers$name[6])
 
 # View the structure and summary of the data
 str(gdb_table1)
+crs(gdb_table1)
+
 summary(gdb_table1)
-plot(gdb_table1)
+plot(gdb_table1$Shape)
+gdb_table1$Shape
 
-gdb_layers$name
 
-for (a in gdb_layers$name) {
+#load grid of NBS and EBS
+load('./extrapolation grids/northern_bering_sea_grid.rda')
+load('./extrapolation grids/eastern_bering_sea_grid.rda')
+grid<-as.data.frame(rbind(data.frame(northern_bering_sea_grid,region='NBS'),data.frame(eastern_bering_sea_grid,region='EBS')))
+grid$cell<-1:nrow(grid)
+grid2<-grid
+
+area<-list()
+
+for (i in 1:length(gdb_layers$name)) {
+  
+  #i<-3
+  
+  #load grid of NBS and EBS
+  load('./extrapolation grids/northern_bering_sea_grid.rda')
+  load('./extrapolation grids/eastern_bering_sea_grid.rda')
+  grid<-as.data.frame(rbind(data.frame(northern_bering_sea_grid,region='NBS'),data.frame(eastern_bering_sea_grid,region='EBS')))
+  grid$cell<-1:nrow(grid)
+  #grid2<-grid
+  coordinates(grid)<-~Lon + Lat
+  proj4string(grid) <- CRS("+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0") 
+  pts<-spTransform(grid,CRSobj = crs(gdb_table1))
+  #pts<-as.data.frame(pts)
+  
+
+  #st to sp
+  gdb_tablea<-as(get(paste0('gdb_table',i)),'Spatial')
+  #plot(pts)
+  plot(gdb_tablea)
+  
+  if (length(gdb_tablea$Shape_Area)!=1) {
+    iarea<-sum(gdb_tablea$Shape_Area/1000000)
+  } else{
+    iarea<-gdb_tablea$Shape_Area/1000000
+  }
+  
+  area[[i]]<-iarea
+    
+  
+  plot(gdb_tablea[2])
+  
+  #points over polygon
+  xx<-over(pts,gdb_tablea)
+  
+  # Check which points fall within the sf object using st_within
+  points_within_polygon <- as.data.frame(pts)[!is.na(xx), 'cell']
+  
+  #name<-
+  grid2$newcolumn<-FALSE
+  grid2$newcolumn[points_within_polygon]<-TRUE
+  names(grid2)[ncol(grid2)]<-gdb_layers$name[i]
+}
+
+area[[1]]<-NULL
+area[[4]]<-area[[5]]
+
+grid2
+
+ggplot()+
+  geom_point(data=grid2,aes(x=Lon,y=Lat,col=EBS_CO_CB_strata))
+
+
+samp<-'scn1'
+#load survey allocations by sampling design
+load(file = paste0('./output/survey_allocations_',samp,'.RData')) #scn_allocations
+dimnames(scn_allocations)[[3]]<-c('sys','rand','sb')
+dimnames(scn_allocations)
+
+
+PBL_KC_cells<-grid2[which(grid2$Pribilof_BKC_strata==TRUE),'cell']
+STM_BKC_cells<-grid2[which(grid2$StMatt_BKC_strata==TRUE),'cell']
+EBS_C_cells<-grid2[which(grid2$EBS_CO_CB_strata==TRUE),'cell']
+
+sur<-1
+apr<-'sys'
+sim<-1
+y<-1
+
+#spp-stock to add
+crabs<-c('PBL_BKC','PBL_RKC','STM_BKC','SNW_CRB','TNR_CRB')
+
+#load data
+load(file = paste0('./output/species/ms_sim_dens.RData'))  #sim_dens1
+#simulated densities of survey and year
+sim_dens2<-sim_dens1[,,1,sim]
+
+scn_allocations[scn_allocations[,'sur',apr]==sur & scn_allocations[,'cell',apr] %in% n,,apr]
+
+
+
+
+#spp-stock to add
+crabs<-c('PBL_BKC','PBL_RKC','STM_BKC','SNW_CRB','TNR_CRB')
+names(area)<-crabs
+crabs_spp<-c('Paralithodes platypus','Paralithodes camtschaticus','Paralithodes platypus','Chionoecetes opilio','Chionoecetes bairdi')
+
+
+for (c in crabs) {
+  
+  c<-crabs[1]
+  
+  if (grepl('PBL',c)) {
+    cells<-PBL_KC_cells
+  } else if (grepl('CRB',c)) {
+    cells<-EBS_C_cells
+  } else {
+    cells<-STM_BKC_cells
+  }
+  
+  crab_sp<-crabs_spp[match(c,crabs)]
+  
+  #get densities based on station allocations
+  sim_survey_crabs<-data.frame(cbind(strata=scn_allocations[scn_allocations[,'sur',apr]==n & scn_allocations[,'cell',apr] %in% cells,c('strata'),apr],
+                               dens=sim_dens2[scn_allocations[scn_allocations[,'sur',apr]==n & scn_allocations[,'cell',apr] %in% cells,c('cell'),apr],crab_sp]),check.names = FALSE)
+  names(sim_survey_crabs)[ncol(sim_survey_crabs)]<-crab_sp
   
   
+  sim_survey1<-reshape2::melt(sim_survey_crabs,id.vars=c('strata'))
+  sim_survey1$strata<-'crab'
   
+  #mean, sum and var by strata and year (variable)
+  sim_survey2<-aggregate(x=sim_survey1$value,
+                         by=list(strata=sim_survey1$strata,sp=sim_survey1$variable),
+                         FUN = function(x) c('mean' = mean(x,na.rm=T), 'sum' = sum(x),'var' = var(x,na.rm=T) ))
+  zzz<-data.frame('strata'=sim_survey2$strata,'sp'=sim_survey2$sp,'mean'=sim_survey2$x[,c('mean')],'var'=sim_survey2$x[,c('var')]) #/length(yy$value)
+  
+  #add index strata for sum to compute index (mean strata density * area of strata) kg!
+  zzz$index_strata<-zzz$mean*area[[c]]
+  
+  #add strata var 
+  zzz$strs_var<-zzz$var*(area[[c]]^2)/nrow(sim_survey_crabs) #sum(survey_detail$Nh) 
+  
+  #get CV across years
+  zzz$cv<- sqrt(zzz$strs_var) / zzz$index_strata
+  
+  #mean CV 
+  mean(zzzz1$cv,na.rm=TRUE)
+  
+  #get outputs
+  STRS_mean <- zzzz1$index_strata
+  STRS_var <- zzzz1$strs_var
+  CV <- sqrt(STRS_var) / STRS_mean
   
 }
 
+
+
+#get densities based on station allocations
+sim_survey<-data.frame(cbind(strata=scn_allocations[scn_allocations[,'sur',apr]==n,c('strata'),apr],
+                             dens=sim_dens2[scn_allocations[scn_allocations[,'sur',apr]==n,c('cell'),apr],]),check.names = FALSE)
+
+sim_survey1<-reshape2::melt(sim_survey,id.vars=c('strata'))
+#mean, sum and var by strata and year (variable)
+sim_survey2<-aggregate(x=sim_survey1$value,
+                       by=list(strata=sim_survey1$strata,sp=sim_survey1$variable),
+                       FUN = function(x) c('mean' = mean(x,na.rm=T), 'sum' = sum(x),'var' = var(x,na.rm=T) ))
+
+
+
+
+print(selected_rows)
 
 ####################################################################
 ####################################################################
@@ -62,13 +219,13 @@ if (!('akgfmaps' %in% installed.packages())) {
 
 #load/install packages
 pacman::p_load(pack_cran,character.only = TRUE)
+out_dir<-'/Users/daniel/Work/Adapting Monitoring to a Changing Seascape/'# Define plot extent (through trial end error) units k
+sur<-1
+apr<-'sys'
 
-#set working directory
-out_dir<-'C:/Users/Daniel.Vilas/Work//Adapting Monitoring to a Changing Seascape/'
-out_dir<-'/Users/daniel/Work/Adapting Monitoring to a Changing Seascape/'
-setwd(out_dir)
-
-# Define plot extent (through trial end error) units km
+sur<-1
+apr<-'sys'
+m
 panel_extent <- data.frame(x = c(-1716559.21, -77636.05), #x = c(-1326559.21, -87636.05),
                            y = c(483099.5, 2194909.7)) #y = c(533099.5, 1894909.7))
 
@@ -85,8 +242,8 @@ gdb_table1
 
 ggplot()+
   geom_sf(data=ebs_layers$survey.strata,fill = NA)+ #gdb_table
-  # geom_sf(data=gdb_table1,fill = 'green',alpha=0.1)+
-  # geom_sf_text(data = gdb_table1, label=gdb_layers$name[1],color='green')+
+   geom_sf(data=gdb_table1,fill = 'green',alpha=0.1)+
+   geom_sf_text(data = gdb_table1, label=gdb_layers$name[1],color='green')+
   # # geom_sf(data=gdb_table2,fill = 'red',alpha=0.1)+
   # # geom_sf_text(data = gdb_table2, label=gdb_layers$name[2],color='red')+
   # geom_sf(data=gdb_table3,fill = 'yellow',alpha=0.1)+
@@ -95,8 +252,8 @@ ggplot()+
   # # geom_sf_text(data = gdb_table4, label=gdb_layers$name[4],color='blue')+
   # geom_sf(data=gdb_table5,fill = 'orange',alpha=0.1)+
   # geom_sf_text(data = gdb_table5, label=gdb_layers$name[5],color='orange')+
-  geom_sf(data=gdb_table6,fill = 'black',alpha=0.1)+
-  geom_sf_text(data = gdb_table6, label=gdb_layers$name[6],color='black')+
+  #geom_sf(data=gdb_table6,fill = 'black',alpha=0.1)+
+  #geom_sf_text(data = gdb_table6, label=gdb_layers$name[6],color='black')+
   #geom_polygon(data=ak_sppoly,aes(x=long,y=lat,group=group),fill = 'grey60')+
   scale_x_continuous(expand = c(0,0),position = 'bottom',
                      breaks = c(-175,-170,-165,-160,-155),sec.axis = dup_axis())+
