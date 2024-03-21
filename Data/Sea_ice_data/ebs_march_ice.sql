6eb3ac7dfa25efed271c5148696eb6d3e130ac49
Lewis Barnett <lewis.barnett@noaa.gov>
2024-03-21 12:59:14 -0700

    moving sea ice analysis to different repo

--- Data/Sea_ice_data/ebs_march_ice.sql
+++
@@ -1,9 +0,0 @@
---sea ice query for Lewis Barnett
---Monthly average for March in the Bering Sea
-select round(avg(sea_ice_fraction),2) mean_march_ice, extract(year from read_date) year
-from afsc.erddap_crw_sst a
-left join afsc.erddap_crw_sst_spatial_lookup b on a.crw_id=b.id
-where ecosystem='Eastern Bering Sea'
-and extract(month from read_date)=3
-group by extract(year from read_date)
-order by extract(year from read_date);
