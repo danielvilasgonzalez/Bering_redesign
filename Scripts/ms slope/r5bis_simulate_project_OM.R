6b347e2abe79106cd98fe6fa1c944ca435c5dea9
Daniel Vilas <danielvilasgonzalez@gmail.com>
2024-11-07 10:25:01 -0800

    remove old sections, correct years and checks

--- Scripts/ms slope/r5bis_simulate_project_OM.R
+++ Scripts/ms slope/r5bis_simulate_project_OM.R
@@ -68,14 +68,9 @@ spp<-c('Limanda aspera',
 #selected species
 
 #read coinvergence and st slope
-df.conv<-read.csv('./tables/slope_ebsnbs_convspp.csv')
-df.conv$slope_mod<-ifelse(df.conv$slope_st=='There is no evidence that the model is not converged','ST',
-                          ifelse(df.conv$slope=='There is no evidence that the model is not converged','non_ST','non_mod'))
-
-
-slp_conv<-df.conv[which(df.conv$slope_mod %in% c('ST','non_ST')),'spp']
-ebsnbs_conv<-df.conv[which(df.conv$EBS_NBS=='There is no evidence that the model is not converged'),'spp']
-
+df_conv<-read.csv('./tables/slope_conv.csv')
+slp_conv<-df_conv[which(df_conv$slope_st=='convergence'),'spp']
+ebsnbs_conv<-df_conv[which(df_conv$EBS_NBS=='convergence'),'spp']
 
 #create folder simulation data
 dir.create(paste0('./output slope//species/'))
@@ -118,197 +113,12 @@ dens_index_hist_OM<-list()
 #                          dim=c(nrow(grid),length(project_yrs),1,nrow(df_sbt),length(spp)),
 #                          dimnames=list(1:nrow(grid),project_yrs,1,1:nrow(df_sbt),spp))
 
-yrs<-c(2002,2004,2008,2010,2012,2016)
-
-######################
-# Simulate historical data
-######################
-
-#loop over spp
-for (sp in spp) {
-  
-  #sp<-spp[5] #20
-  
-  # if (sp %in% c('Atheresthes stomias','Atheresthes evermanni')) {
-  #   yrs<-1991:2022
-  # } else {
-  #   yrs<-1982:2022
-  # }
-  # 
-  
-  mod<-df.conv[which(df.conv$spp==sp),'slope_mod']
-  
-  if (mod=='ST') {
-    
-    mod1<-'fit_st.RData'
-    
-  } else if (mod=='non_ST') {
-    
-    mod1<-'fit.RData'
-    
-  } else {
-    
-    next
-  }
-  
-  #create folder simulation data
-  dir.create(paste0('./output slope/species/',sp,'/'))
-  
-  #get list of fit data
-  ff<-list.files(paste0('./slope EBS VAST/',sp),mod1,recursive = TRUE)
-  
-  #load fit file
-  load(paste0('./slope EBS VAST/',sp,'/',ff)) #fit
-  #getLoadedDLLs() #if check loaded DLLs
-  #check_fit(fit$parameter_estimates)
-  
-  ##reload model
-   fit<-
-     reload_model(x = fit)
-  
-  #store index and dens
-  index<-fit$Report$Index_ctl
-  dens<-fit$Report$D_gct
-  dens_index_hist_OM[[sp]]<-list('index'=index,'dens'=dens)
-  
-  #check observation and predicted densities at each obs
-  #observations
-  # file<-files.5[grep('data_geostat',files.5$name),]
-  # 
-  # #download file
-  # googledrive::drive_download(file=file$id,
-  #                             path = paste0('./shelf EBS NBS VAST/',sp,'/data_geostat_temp.rds'),
-  #                             overwrite = TRUE)
-  # 
-  # data_geostat<-readRDS(paste0('./shelf EBS NBS VAST/',sp,'/data_geostat_temp.rds'))
-  
-  #predictions
-  #d_i<-fit$Report$D_i #nrow(fit$data_frame)
-  #length(d_i)==nrow(data_geostat)
-  
-  #################
-  # get predTF (required argument to get predictions on grid when simulating data)
-  #################
-  
-  #read data_geostat_temp file
-  load(paste0('./slope EBS VAST/',sp,'/data_geostat_temp.RData'))
-  #data_geostat1<-readRDS(paste0('./shelf EBS NBS VAST/',sp,'/data_geostat_temp.rds'))
-  #slope- data
-  #EBSslope- grid
-  
-  data_geostat<-data_geostat1[which(data_geostat1$Region %in% c("slope")),]
-  
-  #rbind grid and data_geostat to get prediction into grid values when simulating data
-  # data_geostat<-data_geostat1[which(data_geostat1$Region %in% c("Eastern Bering Sea Crab/Groundfish Bottom Trawl Survey",
-  #                                                                "Northern Bering Sea Crab/Groundfish Survey - Eastern Bering Sea Shelf Survey Extension")),]
-  
-  #to get predictions in locations but not influencing fit
-  pred_TF <- rep(1, nrow(data_geostat1))
-  pred_TF[1:nrow(data_geostat)] <- 0
-
-  #array to store simulated densities/CPUE
-   sim_dens<-array(NA,
-                   dim=c(nrow(grid),length(unique(yrs)),n_sim_hist),
-                   dimnames=list(1:nrow(grid),unique(yrs),1:n_sim_hist))
-
-  #create folder simulation data
-  dir.create(paste0('./output slope/species/',sp,'/simulated historical data/'))
-
-  for (isim in 1:n_sim_hist) { #simulations
-    
-    #isim<-1
-    
-    #print simulation to check progress
-    cat(paste(" #############   Species", sp, match(sp,spp), 'out of',length(spp),  "  #############\n",
-              " #############  historical simulation", isim, "of",n_sim_hist, " #############\n"))
-    
-    #simulate data from OM
-    Sim1 <- FishStatsUtils::simulate_data(fit = fit, #kg/km2
-                                          type = 1,
-                                          random_seed = isim)
-    
-    # if (sp=='Atheresthes evermanni') {
-    #   #select simulated data that belong to grid points
-    #   sim_bio <-matrix(data = Sim1$b_i[pred_TF == 1], #kg
-    #                    nrow = nrow(grid),
-    #                    ncol = length(c(1991:2019,2021:2022)))
-    #   
-    #   sim_bio <-
-    #       cbind(matrix(NA,nrow = nrow(grid),ncol=length(1982:1990)),
-    #       sim_bio[,c(1:29)],
-    #       matrix(NA,nrow = nrow(grid),ncol=length(2020)),
-    #       sim_bio[,c(30:31)])
-    #   
-    # } else if (sp=='Atheresthes stomias') {
-    #   sim_bio <-matrix(data = Sim1$b_i[pred_TF == 1], #kg
-    #                    nrow = nrow(grid),
-    #                    ncol = length(c(1982:2022)))
-    #   sim_bio[,c(1:9,39)]<-NA
-    # } else{
-      sim_bio <-matrix(data = Sim1$b_i[pred_TF == 1], #kg
-                       nrow = nrow(grid),
-                       ncol = length(c(2002,2004,2008,2010,2012,2016))
-                       )
-      #sim_bio[,c(39)]<-NA
-    #}
-    
-   
-    #biomass (kg) to CPUE (kg/km2)
-    sim_dens[,,isim]<-sim_bio/grid$Area_in_survey_km2
-    
-  }
-
-  #save data
-  save(sim_dens, file = paste0("./output slope/species/",sp,'/simulated historical data/sim_dens_slope.RData'))
-
-  #store
-  sim_hist_dens_spp[,,,sp]<-sim_dens
-}
+yrs<-2002:2016
 
 #save 100 simulated historical densities for all species
 #save(sim_hist_dens_spp, file = paste0("./output/species/sim_hist_dens_spp.RData"))
 #save true densities and index for all species
 #save(dens_index_hist_OM, file = paste0("./output/species/dens_index_hist_OM.RData")) 
-
-######################
-# Reshape simulated historical data
-######################
-
-# Initializing parallel backend
-cl <- makeCluster(detectCores()-1)  # Using all available cores
-registerDoParallel(cl)
-
-#n_sim
-n_sim<-100
-
-#array to store simulated densities/CPUE
-sim_dens1 <- array(NA,
-                   dim = c(nrow(grid), length(spp), length(unique(yrs)), n_sim),
-                   dimnames = list(1:nrow(grid), spp, unique(yrs), 1:n_sim))
-
-#parallel loop over spp
-foreach(sp = slp_conv) %do% {
-  
-  #sp<-spp[1]
-  
-  #load data
-  load(paste0('./output slope/species/', sp, '/simulated historical data/sim_dens_slope.RData'))
-  
-  #parallel loop over years and simulations
-  foreach(y = yrs) %:%
-    foreach(sim = 1:n_sim) %do% {
-      #y<-'1982';sim<-'1'
-      
-      #store results
-      sim_dens1[, sp, as.character(y), as.character(sim)] <- sim_dens[, as.character(y), as.character(sim)]
-    }
-}
-
-# Stopping the parallel backend
-stopCluster(cl)
-
-#store HIST simulated data
-save(sim_dens1, file = paste0('./output slope//species/ms_sim_dens_slope.RData'))  
 
 ######################
 # Simulate historic data for EBS and NBS for new species
@@ -319,31 +129,6 @@ load('./extrapolation grids/northern_bering_sea_grid.rda')
 load('./extrapolation grids/eastern_bering_sea_grid.rda')
 grid<-as.data.frame(rbind(data.frame(northern_bering_sea_grid,region='NBS'),data.frame(eastern_bering_sea_grid,region='EBS')))
 grid$cell<-1:nrow(grid)
-
-#selected species not previously in the EBS - NBS
-spp<-c(#'Limanda aspera',
-       #'Gadus chalcogrammus',
-       #'Gadus macrocephalus',
-       #'Atheresthes stomias',
-       'Reinhardtius hippoglossoides',
-       #'Lepidopsetta polyxystra',
-       #'Hippoglossoides elassodon',
-       #'Pleuronectes quadrituberculatus',
-       #'Hippoglossoides robustus',
-       #'Boreogadus saida',
-       #'Eleginus gracilis',
-       'Anoplopoma fimbria',
-       #'Chionoecetes opilio',
-       #'Paralithodes platypus',
-       #'Paralithodes camtschaticus',
-       #'Chionoecetes bairdi',
-       'Sebastes alutus',
-       'Sebastes melanostictus',
-       #'Atheresthes evermanni',
-       'Sebastes borealis',
-       'Sebastolobus alascanus',
-       'Glyptocephalus zachirus',
-       'Bathyraja aleutica')
 
 #yrs
 yrs<-c(1982:2022)
@@ -356,8 +141,8 @@ sim_hist_dens_spp<-array(NA,
 # Simulate historical data
 ######################
 
-#loop over spp
-for (sp in spp) {
+#loop over spp that were not done in the first manuscript
+for (sp in spp[c(17,18,21,23)]) {
   
   #sp<-spp[1] #20
   
@@ -518,7 +303,7 @@ spp<-c('Limanda aspera',
        'Bathyraja aleutica')
 
 # Initializing parallel backend
-cl <- makeCluster(2)  # Using all available cores
+cl <- makeCluster(1)
 registerDoParallel(cl)
 
 #n_sim
@@ -552,6 +337,7 @@ stopCluster(cl)
 
 #store HIST simulated data
 save(sim_dens1, file = paste0('./output slope//species/ms_sim_dens.RData'))  
+#load(file = paste0('./output slope//species/ms_sim_dens.RData'))  
 
 ########################################################################################
 # JOIN EBS+NBS and SLP ARRAY
@@ -564,8 +350,9 @@ ebsnbs_simdens<-sim_dens1
 #sim_dens1
 load(file = paste0('./output slope//species/ms_sim_dens_slope.RData'))  
 slp_simdens<-sim_dens1
+dimnames(slp_simdens)
 
-# Replace NAs with 0 in the 4D array
+# Replace NAs with 0 in the 4D array for species that are not present in the region
 ebsnbs_simdens[is.na(ebsnbs_simdens)] <- 0
 slp_simdens[is.na(slp_simdens)] <- 0
 
@@ -595,10 +382,6 @@ spp<-c('Limanda aspera',
        'Glyptocephalus zachirus',
        'Bathyraja aleutica')
 
-# # Initializing parallel backend
-# cl <- makeCluster(2)  # Using all available cores
-# registerDoParallel(cl)
-
 #n_sim
 n_sim<-100
 
@@ -616,7 +399,7 @@ for (sp in spp) {
     for (sim in 1:n_sim) {
       
       
-      if (y %in% c(2002,2004,2008,2010,2012,2016)) {
+      if (y %in% c(2002:2016)) {
         #store results
         sim_dens1[, sp, as.character(y), as.character(sim)] <-c(ebsnbs_simdens[,sp,as.character(y), as.character(sim)],
                                                                     slp_simdens[,sp,as.character(y), as.character(sim)])
@@ -631,3 +414,9 @@ for (sp in spp) {
 #store HIST simulated data
 save(sim_dens1, file = paste0('./output slope//species/ms_sim_dens_all.RData'))  
 
+#checks
+dimnames(sim_dens1)
+sim_dens1[53465:53470,'Gadus macrocephalus', '2000', '1'] #should be NA
+sim_dens1[1:20,'Sebastolobus alascanus', '2000', '1'] #should be 0
+sim_dens1[53465:53470,'Gadus macrocephalus', '2002', '1'] #should be positive
+sim_dens1[53465:53470,'Gadus macrocephalus', '2003', '1'] #should be positive
